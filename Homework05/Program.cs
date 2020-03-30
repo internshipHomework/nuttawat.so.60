ff80815be7b1c187a31f7ef85c4a92973dd372c2
nuttawatso <nuttawat.so.60@ubu.ac.th>
2020-03-30 14:05:53 +0700

    30-03-63

---
+++ Homework05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Homework05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] LED = { "1","2","3","4","5","6","7","8","9","A" };
+            string[] Light = { "[ปิด]","[ปิด]","[ปิด]","[ปิด]","[ปิด]","[ปิด]","[ปิด]","[ปิด]","[ปิด]","[ปิด]" };
+            for (int round = 1; round <=50 ; round++){
+                Console.Write("Please choose LED to turn On/Off:");
+                string input = Console.ReadLine();
+                for (int i=0; i<10 ; i++){
+                    if( LED[i] == input & Light[i] == "[ปิด]" ){
+                        Light[i] = "[ไฟเปิด]";
+                    }
+                    else if (LED[i] == input & Light[i] == "[ไฟเปิด]"){
+                        Light[i] = "[ปิด]";
+                    }
+                
+                }
+                Console.WriteLine("  1    2    3    4    5    6    7    8    9    A ");
+                Console.WriteLine(string.Join(" ", Light)) ;
+             
+            }
+
+        }
+    }
+}
---
+++ Homework09/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Collections;
+using System;
+
+namespace Homework09
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var reader = new StreamReader(@"product.csv"))
+            {
+                List<string> SKU = new List<string>();
+                List<string> Name = new List<string>();
+                List<string> Price = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    SKU.Add(values[0]);
+                    Name.Add(values[1]);
+                    Price.Add(values[2]);
+                }
+                Console.WriteLine("Products in your cart are");
+                Console.WriteLine("none");
+                Console.WriteLine("----------");
+                Console.WriteLine("Total amount: 0.00 Bath");
+
+                ArrayList Product = new ArrayList();
+                int sum = 0 ;
+
+                do{
+                    Console.WriteLine("Please input a product key :");
+                    string input = Console.ReadLine();
+                    for (int i = 0 ; i < SKU.Count; i++ ) {
+                        if ( SKU[i] == input ){
+                            string v = Price[i];
+                            int x = Convert.ToInt32(v);
+                            sum += x;
+                            Product.Add(Name[i] + "  " + v);
+
+                        }
+                      
+                    }
+                
+                    Console.WriteLine("Products in your cart are");
+                    for (int i = 1  ; i <= Product.Count; i++){
+                        int Number = Convert.ToInt32(i);
+                        int Order = Number - 1;
+
+                        Console.WriteLine(Number + ". " + Product[Order]);
+                    }
+          
+                    Console.WriteLine("----------");
+                    Console.WriteLine("Total amount: " + sum + " Bath");
+
+                } while (true);
+                
+            
+            }
+        }
+    }
+}
+
+
+
+
